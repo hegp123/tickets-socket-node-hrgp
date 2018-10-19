837672098059f4485104c79bf2e010c8b9e25829
Hector Garcia <hectoregarciap@hotmail.com>
2018-10-19 16:48:41 -0500

    Produccion

---
+++ public/js/socket.escritorio.js
@@ -0,0 +1,26 @@
+// comando para establecer la nocexion
+var socket = io();
+
+var searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es necesario');
+}
+
+var escritorio = searchParams.get('escritorio');
+var label = $('small');
+
+console.log(escritorio);
+$('h1').text('Escritorio ' + escritorio);
+
+$('button').on('click', function() {
+    socket.emit('atenderTicket', { escritorio: escritorio }, function(resp) {
+        if (resp === 'No hay tickets') { // el mismo nombre que esta en el archivo ticket-control.js  metodo: atenderTicket
+            label.text(resp);
+            alert(resp);
+            return;
+        }
+        label.text('Ticket ' + resp.numero);
+    });
+});
